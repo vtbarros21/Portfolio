961bc229f7f2a5b9cf3df4dfeb5ced515fb21c0a
Victor Hugo <vete2260@gmail.com>
2023-12-07 23:07:48 -0300

    :dizzy: added animations to the mobile hamburger menu

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,16 +1,42 @@
 import s from "../styles.module.sass";
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links = () => {
-  const items = ["Inicio","Sobre Mim", "Projetos", "Serviços", "Skills"];
+  const items = ["Inicio", "Sobre Mim", "Projetos", "Serviços", "Skills"];
 
   return (
-    <div className={s.Links}>
+    <motion.div className={s.Links} variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{scale:1.1}} whileTap={{scale:0.95}}>
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
