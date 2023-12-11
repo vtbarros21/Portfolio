ed64462b0603eab24a5dede7c513e4145464c0da
Victor Hugo <vete2260@gmail.com>
2023-12-11 16:44:13 -0300

    :bug: fix: solving bug with route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RoutesApp from "./routes/routes";
-import "./styles/main.sass";
+import "./styles/Main.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
