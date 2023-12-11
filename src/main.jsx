4a55aae3b6d12e456f17a89d7ea842028dac1bfa
Victor Hugo <vete2260@gmail.com>
2023-12-11 16:42:20 -0300

    :bug: fix: solving bug with route

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RoutesApp from "./Routes/routes";
+import RoutesApp from "./routes/routes";
 import "./styles/main.sass";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
